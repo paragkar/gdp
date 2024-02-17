a1ee70c8952df31750618870e3e61b6a7d8946b9
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-02-17 11:46:26 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -2,4 +2,10 @@
 import pandas as pd
 import streamlit as st
 
-st.write("Hello World")
+st.set_page_confit(
+	page_tile = "Multipage App", 
+	page_icon ='$',
+	)
+
+st.title("Main Page")
+st.sidebar.sucess("Select a page abobe.")
