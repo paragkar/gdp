9080615d0707e47df1dfbf637962e7d22edac1fe
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-02-22 14:25:28 +0530

    Update gdp.py

--- gdp.py
+++ gdp.py
@@ -76,9 +76,9 @@ def process_df_choosen_timescale(df,timescale, feature):
             dftemp2 = df.groupby(["FYear", "Description"]).agg({"Value": "sum"}).reset_index()
             dftemp = dftemp2.merge(dftemp1, on =["FYear"], how ='left')
             dftemp = df.merge(dftemp, on =["FYear"], how = 'left')
+            dftemp["Value"] = (dftemp["Value"]/dftemp["Value_y"])*100
+            dftemp = dftemp.rename(columns = {"Description_x":"Description"})
             st.write(dftemp)
-            dftemp["Value"] = (dftemp["Value_x"]/dftemp["Value_y"])*100
-    
             pivot_df = dftemp.pivot_table(index='Description', columns='FYear', values='Value')
         
     #sorting the dataframe 
