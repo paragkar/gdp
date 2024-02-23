3c1699b6be87ac89e8bf513aadb1e3d9a6a1df2d
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-02-23 10:52:11 +0530

    Update gdp.py

--- gdp.py
+++ gdp.py
@@ -99,8 +99,8 @@ def create_heatmap_data(df, hovertext, texttemplate):
     Q1 = np.percentile(z_values, 25)
     Q3 = np.percentile(z_values, 75)
     IQR = Q3 - Q1
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
+    lower_bound = Q1 - 1 * IQR
+    upper_bound = Q3 + 1 * IQR
 
 
     data = [go.Heatmap(
