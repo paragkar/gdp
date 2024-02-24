a92b78ac617d7111c584a1219910a168c3b68c2d
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-02-24 09:43:35 +0530

    Update gdp.py

--- gdp.py
+++ gdp.py
@@ -278,7 +278,7 @@ if pivot_df.shape[0] != 0:
     # coltotaldf = pivot_df.sum(axis=0).round(1).reset_index()
     # coltotaldf.columns =[timescale, dimension]
     # bar_data = create_bar_chart_data(coltotaldf, timescale, dimension)
-    total_df = total_df.T
+    total_df = total_df.T.reset_index()
     total_df.columns =[timescale, dimension]
     bar_data = create_bar_chart_data(totaldf, timescale, dimension)
     fig2 = go.Figure(data=bar_data)
