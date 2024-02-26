41dffd3f1b5e3449793315e2727364a937f75520
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-02-26 15:23:11 +0530

    Update gdp.py

--- gdp.py
+++ gdp.py
@@ -273,7 +273,7 @@ def createslider(pivot_df):
 #load data
 df = loadgdpgva()
 
-st.write(df[df["Date"]==max(df["date"])])
+st.write(df[df["Date"]==max(df["Date"])])
 
 #choose a dimension
 dimension = st.sidebar.selectbox('Select a Dimension', ["GDP Current", "GDP Constant", "GVA Current","GVA Constant"])
