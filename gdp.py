d09cc0238d260d4b6f2d3432fdaaaf52b5b52229
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-02-26 16:04:30 +0530

    Update gdp.py

--- gdp.py
+++ gdp.py
@@ -294,19 +294,14 @@ pivot_df = processing_currency(dimension, curreny, timescale, feature, df)
 if (feature != "Growth"):
     pivot_df = pivot_df.sort_values(pivot_df.columns[-1], ascending = True)
 if (feature == "Growth") & ((dimension == "GDP Constant") | (dimension == "GDP Current")):
-    st.write(pivot_df.index)
     pivot_df.index = pd.Categorical(pivot_df.index, categories=lst_for_sorting_pivot_df1, ordered=True)
-    st.write(pivot_df.index)
     pivot_df = pivot_df.sort_index()
     pivot_df.index = pivot_df.index.astype('object')
-    st.write(pivot_df.index)
 if (feature == "Growth") & ((dimension == "GVA Constant") | (dimension == "GVA Current")):
-    st.write(pivot_df.index)
     pivot_df.index = pd.Categorical(pivot_df.index, categories=lst_for_sorting_pivot_df1, ordered=True)
     pivot_df = pivot_df.sort_index()
+    pivot_df.index = pivot_df.index.astype('object')
 
-st.write(lst_for_sorting_pivot_df1)
-st.write(pivot_df)
 
 
 filter_desc = dimension.split(" ")[0]
