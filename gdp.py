5307aa80dc4bf5f7c72b3b37d767b6cedb6ce92a
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-02-28 12:41:06 +0530

    Update gdp.py

--- gdp.py
+++ gdp.py
@@ -452,7 +452,7 @@ if plot_type == "Heatmap":
 if plot_type == "Scatter":
 
 
-    st.write(pivot_df.iloc[0,:].apply(lambda x: x*-1))
+    pivot_df.iloc[0,:] = pivot_df.iloc[0,:].apply(lambda x: x*-1)
 
     try:
 
