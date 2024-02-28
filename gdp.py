b4b0bff7ed7da588692f0fb8e691508761be97d8
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-02-28 11:48:00 +0530

    Update gdp.py

--- gdp.py
+++ gdp.py
@@ -502,7 +502,7 @@ if plot_type == "Scatter":
         fig.add_shape(
             type="rect",
             xref="paper", yref="paper",
-            x0=-0.047, y0=-0.047,
+            x0=-0.046, y0=-0.046,
             x1=1, y1=1,
             line=dict(color="Black", width=2),
         )
