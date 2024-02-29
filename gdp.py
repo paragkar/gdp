ba5dd6e9159ac1b34912af630c3b73952585cc22
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-02-29 10:12:18 +0530

    Update gdp.py

--- gdp.py
+++ gdp.py
@@ -76,10 +76,10 @@ def process_df_choosen_timescale(df,timescale, feature):
             pivot_df = dftemp.pivot_table(index='Description', columns='Date', values='Value')
         if feature == "Growth":
 
-            st.write(df) # debug
-
             pivot_df = df.pivot_table(index='Description', columns='Date', values='Value')
             pivot_df = ((pivot_df - pivot_df.shift(5, axis =1))/pivot_df.shift(5, axis =1))*100
+
+            st.write(pivot_df) #debug
            
     if timescale == "FYear":
         if feature == "Absolute":
