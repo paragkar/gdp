ed66d12ade0c102c017f5b7875be848524585f96
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-02-29 15:16:36 +0530

    Update gdp.py

--- gdp.py
+++ gdp.py
@@ -635,7 +635,7 @@ def plotingscatterforecast(pivot_df, dimension, timescale, currency, feature, fo
         forecasted_end_value = all_y_data[-1]
 
         # Calculate the sequential growth rate
-        growth_rate = ((forecasted_end_value / last_actual_value) ** (1 / forecast_period) - 1)*100
+        growth_rate = ((forecasted_end_value / last_actual_value) ** (1 / forecast_period) - 1)
 
         # Generate custom hover text including the growth rate
         hover_text = [f"{dim}<br>Date: {str(date.date())}<br>Value: {value}<br>Required Growth: {growth_rate:.2%}"
